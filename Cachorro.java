b0bd1b9adcbdcf3e144f84123627d504b2e9b5af
Per1n1 <151886360+Per1n1@users.noreply.github.com>
2024-05-19 20:40:42 -0300

    Create Cachorro.java

---
+++ Cachorro.java
@@ -0,0 +1,11 @@
+package br.unipar;
+
+public class Cachorro extends Sobrescrita {
+        @Override
+        public void fazerSom() {
+            System.out.println("Au au!");
+        }
+    }
+
+
+    //estaria dentro de sobrescrita
