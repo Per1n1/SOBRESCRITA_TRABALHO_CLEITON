b024337e87619428f618fb5e32523fcd706e8f1b
Per1n1 <151886360+Per1n1@users.noreply.github.com>
2024-05-19 21:05:55 -0300

    Create SobrecargaTeste.java

---
+++ SobrecargaTeste.java
@@ -0,0 +1,14 @@
+import br.unipar.Sobrecarga;
+import org.junit.Test;
+
+public class SobrecargaTest {
+    @Test
+    public  void testesomar (){
+
+        Sobrecarga somar = new Sobrecarga();
+        int somar1 = somar.somar(12, 16);
+
+    }
+
+
+}
